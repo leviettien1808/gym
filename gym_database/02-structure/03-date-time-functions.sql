949e9a39bda5b939d3ed78738765bcf90b7bffa3
leviettien1808 <leviettien1808@gmail.com>
2025-08-12 19:21:14 +0700

    Working with Weekdays

--- gym_database/02-structure/03-date-time-functions.sql
+++ gym_database/02-structure/03-date-time-functions.sql
@@ -8,6 +8,26 @@ FROM memberships;
 SELECT EXTRACT(MINUTE FROM last_checkin), last_checkin
 FROM memberships;
 
+-- MySQL
+SELECT WEEKDAY(last_checkin), last_checkin -- Monday is 0
+FROM memberships;
+
+SELECT WEEKDAY(last_checkin) + 1, last_checkin -- Monday is 1
+FROM memberships;
+
+SELECT CONVERT(last_checkin, DATE), CONVERT(last_checkin, TIME)
+FROM memberships;
+
+-- Postgres
+SELECT EXTRACT(DOW FROM last_checkin), last_checkin -- Sunday is 0
+FROM memberships;
+
+SELECT EXTRACT(ISODOW FROM last_checkin), last_checkin -- Sunday is 7
+FROM memberships;
+
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
+FROM memberships;
+
 -- SQLite
 SELECT strftime('%m', last_checkin), last_checkin
 FROM memberships;
@@ -16,4 +36,10 @@ SELECT strftime('%d', last_checkin), last_checkin
 FROM memberships;
 
 SELECT strftime('%M', last_checkin), last_checkin
+FROM memberships;
+
+SELECT strftime('%w', last_checkin), last_checkin -- Sunday is 0
+FROM memberships;
+
+SELECT date(last_checkin), time(last_checkin)
 FROM memberships;
