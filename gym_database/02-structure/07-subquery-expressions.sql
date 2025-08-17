af438caca7ec735b14671ff8048a555e1fb00702
leviettien1808 <leviettien1808@gmail.com>
2025-08-17 09:49:54 +0700

    Subquery expression and EXISTS

--- gym_database/02-structure/07-subquery-expressions.sql
+++ gym_database/02-structure/07-subquery-expressions.sql
@@ -7,3 +7,11 @@ SELECT EXISTS (
     FROM customers
     WHERE email = 'john@gmail.com'
 );
+
+SELECT o.id
+FROM orders AS o
+WHERE EXISTS (
+    SELECT *
+    FROM customers AS c
+    WHERE o.customer_id = c.id AND c.email = 'john@gmail.com'
+);
