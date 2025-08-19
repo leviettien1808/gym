9519ada7f7e8d5e9fb37cde5b4a2607e1c4739a2
leviettien1808 <leviettien1808@gmail.com>
2025-08-19 16:32:20 +0700

    Transactions with Postgres

--- gym_database/02-structure/10.1-start-transactions.sql
+++ gym_database/02-structure/10.1-start-transactions.sql
@@ -1,1 +1,15 @@
+-- MySQL
 START TRANSACTION;
+
+-- Postgres
+BEGIN;
+
+WITH new_customer AS (
+  INSERT INTO customers (first_name, last_name, email)
+  VALUES ('Jonas', 'Blue', 'jonas@test.com')
+  RETURNING id
+)
+INSERT INTO orders (amount_billed, customer_id)
+SELECT 103.12, id FROM new_customer;
+
+COMMIT;
