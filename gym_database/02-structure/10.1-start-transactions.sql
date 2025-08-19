3c23a239f355b202b7ec223d856c2cb405cd721c
leviettien1808 <leviettien1808@gmail.com>
2025-08-19 16:53:36 +0700

    Transactions with SQLite

--- gym_database/02-structure/10.1-start-transactions.sql
+++ gym_database/02-structure/10.1-start-transactions.sql
@@ -13,3 +13,14 @@ INSERT INTO orders (amount_billed, customer_id)
 SELECT 103.12, id FROM new_customer;
 
 COMMIT;
+
+-- SQLite
+BEGIN TRANSACTION;
+
+INSERT INTO customers (first_name, last_name, email)
+VALUES ('Jonas', 'Blue', 'jonas@test.com');
+
+INSERT INTO orders (amount_billed, customer_id)
+VALUES (103.12, last_insert_rowid());
+
+COMMIT;
